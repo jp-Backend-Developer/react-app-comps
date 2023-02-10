f98cc0312e45b8abde34f7ccb6486ef96b8710f3
taishi-yamada <taishi.37.0327@gmail.com>
2023-02-10 09:32:50 +0900

    add table comp

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,20 @@
-function Table({ data }) {
+function Table({ data, config, keyFn }) {
 
-    const renderedRows = data.map((fruit) => {
+    const renderedHeaders = config.map((column) => {
         return (
-            <tr className="border-b" key={fruit.name}>
-                <td className="p-3">{fruit.name}</td>
-                <td className="p-3">
-                    <div className={`p-3 m-2 ${fruit.color}`}></div>
-                </td>
-                <td className="p-3">{fruit.score}</td>
+            <th key={column.label}>{column.label}</th>
+        );
+    })
+
+    const renderedRows = data.map((rowData) => {
+        const renderedCells = config.map((column) => {
+            return (
+                <td className="p-3" key={column.label}>{column.render(rowData)}</td>
+            );
+        });
+        return (
+            <tr className="border-b" key={keyFn(rowData)}>
+                {renderedCells}
             </tr>
         )
     });
@@ -15,11 +22,7 @@ function Table({ data }) {
     return (
         <table className="table-auto border-spacing-2">
             <thead>
-                <tr className="border-b-2">
-                    <th>Fruit</th>
-                    <th>Color</th>
-                    <th>Score</th>
-                </tr>
+                <tr className="border-b-2">{renderedHeaders}</tr>
             </thead>
             <tbody>{renderedRows}</tbody>
         </table>
--- src/pages/TablePages.js
+++ src/pages/TablePages.js
@@ -6,11 +6,21 @@ function TablePages() {
         { name: 'Apple', color:'bg-red-500', score:3},
         { name: 'Banana', color:'bg-yellow-500', score:1},
         { name: 'Lime', color:'bg-green-500', score:4},
-    ]
+    ];
+
+    const config = [
+        {label: 'Name', render: (fruit) => fruit.name},
+        {label: 'Color', render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />},
+        {label: 'Score', render: (fruit) => fruit.score},
+    ];
+
+    const keyFn = (fruit) => {
+        return fruit.name;
+    }
 
     return (
-        <Table data={data} />
-    )
+        <Table data={data} config={config} keyFn={keyFn} />
+    );
 }
 
 export default TablePages;
